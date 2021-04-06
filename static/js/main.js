c06ac2b6f8806c5cb57add1f5fedc9532399bf00
Sparkenstein <24642451+Sparkenstein@users.noreply.github.com>
2021-04-06 21:59:26 +0530

    feat: on click fork and save

--- static/js/main.js
+++ static/js/main.js
@@ -1,21 +1,20 @@
 // deno-lint-ignore-file
 function create(data, language) {
-  return fetch("/", {
-    method: "POST",
-    headers: { "X-Language": language },
-    body: data,
-  }).then(function (res) {
-    return res.text().then(function (text) {
-      console.log("returned", text);
-      if (res.status !== 200) {
-        throw new Error(text);
-      }
-      var uuidBytes = text.split(" ");
-      var uuid = uuidBytes[0];
-      var bytes = uuidBytes[1];
-      return [uuid, bytes];
+    return fetch("/", {
+        method: "POST",
+        headers: { "X-Language": language },
+        body: data,
+    }).then(function (res) {
+        return res.text().then(function (text) {
+            if (res.status !== 200) {
+                throw new Error(text);
+            }
+            var uuidBytes = text.split(" ");
+            var uuid = uuidBytes[0];
+            var bytes = uuidBytes[1];
+            return [uuid, bytes];
+        });
     });
-  });
 }
 
 // const langs = [
@@ -93,133 +92,138 @@ var main = document.querySelector("#main");
 var isNew = location.pathname === "/";
 
 function setLang(e) {
-  editor.trigger("Source", "vs.editor.ICodeEditor:1:set-language");
+    editor.trigger("Source", "vs.editor.ICodeEditor:1:set-language");
+}
+
+function fork() {
+    if (isNew) {
+        return;
+    }
+    history.pushState({}, document.title, "/");
+    editor.updateOptions({ readOnly: false });
+    isNew = true;
+}
+
+function save() {
+    if (!isNew) {
+        return;
+    }
+    var data = editor.getValue();
+    var language = ((editor.getModel() || {})._languageIdentifier || {})
+        .language;
+    create(data, language)
+        .then(function (uuidBytes) {
+            var uuid = uuidBytes[0];
+            var bytes = uuidBytes[1];
+            localStorage.removeItem("paste");
+            location.href = "/" + uuid;
+            return;
+        })
+        .catch(function (err) {
+            var msg = err.message.startsWith("File already exists: ")
+                ? "File already exists at " +
+                  location.href +
+                  err.message.split(" ").pop()
+                : err.message;
+            editor.setValue(editor.getValue() + "\n// Error: " + msg);
+        });
 }
 
 require(["vs/editor/editor.main"], function () {
-  if (!(main instanceof HTMLElement)) {
-    return;
-  }
-  var editor = monaco.editor.create(main, {
-    renderWhitespace: "all",
-    theme: "vs-dark",
-    wordWrap: "on",
-    readOnly: !isNew,
-    theme: "vs-dark",
-  });
-  window.editor = editor;
-  window.addEventListener("resize", () => {
-    editor.layout();
-  });
-  // Get cursor
-  editor.onDidChangeCursorPosition((e) => {
-    document.getElementById(
-      "lines"
-    ).innerText = `Ln ${e.position.lineNumber}, Col ${e.position.column}`;
-  });
-  document.addEventListener("keydown", function (e) {
-    if (!(e.ctrlKey || e.metaKey)) {
-      return;
-    }
-    if (e.code === "KeyS") {
-      if (!isNew) {
+    if (!(main instanceof HTMLElement)) {
         return;
-      }
-      e.preventDefault();
-      var data = editor.getValue();
-      var language = ((editor.getModel() || {})._languageIdentifier || {})
-        .language;
-      console.log(
-        "Got language",
-        language,
-        monaco.editor.model,
-        editor._configuration._rawOptions.language
-      );
-      create(data, language)
-        .then(function (uuidBytes) {
-          var uuid = uuidBytes[0];
-          var bytes = uuidBytes[1];
-          console.log(uuid, bytes);
-          localStorage.removeItem("paste");
-          location.href = "/" + uuid;
-          return;
+    }
+    var editor = monaco.editor.create(main, {
+        renderWhitespace: "all",
+        theme: "vs-dark",
+        wordWrap: "on",
+        readOnly: !isNew,
+        theme: "vs-dark",
+    });
+    window.editor = editor;
+    window.addEventListener("resize", () => {
+        editor.layout();
+    });
+    // Get cursor
+    editor.onDidChangeCursorPosition((e) => {
+        document.getElementById(
+            "lines"
+        ).innerText = `Ln ${e.position.lineNumber}, Col ${e.position.column}`;
+    });
+    document.addEventListener("keydown", function (e) {
+        if (!(e.ctrlKey || e.metaKey)) {
+            return;
+        }
+        if (e.code === "KeyS") {
+            if (!isNew) {
+                return;
+            }
+            e.preventDefault();
+            save();
+            return;
+        }
+        if (e.code === "KeyF" && e.shiftKey) {
+            if (isNew) {
+                return;
+            }
+            e.preventDefault();
+            fork();
+            return;
+        }
+        localStorage.setItem("paste", editor.getValue());
+    });
+    editor.addAction({
+        id: "set-language",
+        label: "Set language",
+        keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_L],
+        run: function (ed) {
+            var lang = prompt("Set language");
+            var model = ed.getModel();
+            if (!model || !lang) {
+                return;
+            }
+            monaco.editor.setModelLanguage(model, lang);
+            localStorage.setItem("language", lang);
+            document.getElementById("setlang").innerText = lang;
+        },
+    });
+    if (!isNew) {
+        var fileLang = location.pathname.split(".");
+        var file = fileLang[0];
+        var lang =
+            fileLang[1] ||
+            document
+                .querySelector('meta[http-equiv="X-Language"]')
+                .getAttribute("content");
+        if (lang) {
+            var model = editor.getModel();
+            if (model) {
+                monaco.editor.setModelLanguage(model, lang);
+            }
+        }
+        editor.setValue("// Loading...");
+        fetch(file, {
+            headers: {
+                Accept: "text/plain",
+            },
         })
-        .catch(function (err) {
-          var msg = err.message.startsWith("File already exists: ")
-            ? "File already exists at " +
-              location.href +
-              err.message.split(" ").pop()
-            : err.message;
-          editor.setValue(editor.getValue() + "\n// Error: " + msg);
-        });
-      return;
+            .then(function (res) {
+                return res.text();
+            })
+            .then(function (text) {
+                editor.setValue(text);
+            });
+    } else {
+        var stored = localStorage.getItem("paste");
+        if (stored) {
+            editor.setValue(stored);
+        }
+        var lang = localStorage.getItem("language");
+        var model = editor.getModel();
+        if (lang) {
+            if (model) {
+                monaco.editor.setModelLanguage(model, lang);
+            }
+        }
     }
-    if (e.code === "KeyF" && e.shiftKey) {
-      if (isNew) {
-        return;
-      }
-      e.preventDefault();
-      history.pushState({}, document.title, "/");
-      editor.updateOptions({ readOnly: false });
-      isNew = true;
-      return;
-    }
-    localStorage.setItem("paste", editor.getValue());
-  });
-  editor.addAction({
-    id: "set-language",
-    label: "Set language",
-    keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_L],
-    run: function (ed) {
-      var lang = prompt("Set language");
-      var model = ed.getModel();
-      if (!model || !lang) {
-        return;
-      }
-      monaco.editor.setModelLanguage(model, lang);
-      localStorage.setItem("language", lang);
-      document.getElementById("setlang").innerText = lang;
-    },
-  });
-  if (!isNew) {
-    var fileLang = location.pathname.split(".");
-    var file = fileLang[0];
-    var lang =
-      fileLang[1] ||
-      document
-        .querySelector('meta[http-equiv="X-Language"]')
-        .getAttribute("content");
-    console.log("fetching paste", file, lang);
-    if (lang) {
-      var model = editor.getModel();
-      if (model) {
-        monaco.editor.setModelLanguage(model, lang);
-      }
-    }
-    editor.setValue("// Loading...");
-    fetch(file, {
-      headers: {
-        Accept: "text/plain",
-      },
-    })
-      .then(function (res) {
-        return res.text();
-      })
-      .then(function (text) {
-        console.log("Fetched", text);
-        editor.setValue(text);
-      });
-  } else {
-    var stored = localStorage.getItem("paste");
-    if (stored) {
-      editor.setValue(stored);
-    }
-    var lang = localStorage.getItem("language");
-    var model = editor.getModel();
-    if (lang) {
-      if (model) {
-        monaco.editor.setModelLanguage(model, lang);
-      }
-    }
-  }
 });
