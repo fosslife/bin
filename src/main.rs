9274eef4b1ab35e8b24d9995130d408fb39e1d95
Sparkenstein <zetabytes.pp@gmail.com>
2022-04-02 13:34:23 +0530

    feat: move server from tide to axum

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,40 @@
-use askama_tide::askama::Template;
-
-use tide::{prelude::*, Request};
-use tide::{Body, Response};
-
-use async_std::{
-    fs::{self, OpenOptions},
-    io,
+use askama::Template;
+use axum::{
+    extract::Path,
+    // extract,
+    http::{HeaderMap, StatusCode},
+    response::{Html, IntoResponse, Response},
+    routing::{get, get_service},
+    Router,
 };
-use std::{borrow::Cow, fmt};
-use tide::StatusCode;
-
 use nanoid;
+use serde::{Deserialize, Serialize};
+use std::{borrow::Cow, fmt, fs, net::SocketAddr};
+use tower_http::{services::ServeDir, trace::TraceLayer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[derive(Template)]
 #[template(path = "index.html")]
-struct Index<'a> {
-    language: &'a str,
+struct Index {
+    language: String,
+}
+
+struct HtmlTemplate<T>(T);
+
+impl<T> IntoResponse for HtmlTemplate<T>
+where
+    T: Template,
+{
+    fn into_response(self) -> Response {
+        match self.0.render() {
+            Ok(html) => Html(html).into_response(),
+            Err(err) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("Failed to render template. Error: {}", err),
+            )
+                .into_response(),
+        }
+    }
 }
 
 pub struct PasteId<'a>(Cow<'a, str>);
@@ -39,66 +58,70 @@ struct PasteBody {
     content: String,
 }
 
-#[async_std::main]
-async fn main() -> tide::Result<()> {
-    tide::log::start();
-    fs::create_dir_all("pastes/metadata").await?;
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "binrs=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
-    let mut app = tide::new();
-    app.at("/").get(index);
-    app.at("/").post(create_paste);
-    app.at("/:id").get(retrieve_paste);
-    app.at("/static").serve_dir("static/")?;
-    app.at("/favicon.ico")
-        .serve_file("static/img/favicon.ico")?;
+    fs::create_dir_all("pastes/metadata").unwrap();
 
-    app.listen("127.0.0.1:3000").await?;
-    Ok(())
+    // Axum:
+    let app = Router::new()
+        .route("/", get(index).post(create))
+        .route("/api/:id", get(retrieve_paste))
+        .route("/:id", get(retrieve_paste_doc))
+        .nest(
+            "/static",
+            get_service(ServeDir::new("static")).handle_error(|error: std::io::Error| async move {
+                (
+                    StatusCode::INTERNAL_SERVER_ERROR,
+                    format!("Unhandled internal error: {}", error),
+                )
+            }),
+        )
+        .layer(TraceLayer::new_for_http());
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
 
-async fn index(_req: Request<()>) -> Result<Response, tide::Error> {
-    let param = "";
-    let res: Response = Index { language: param }.into();
-    Ok(res)
+async fn index() -> impl IntoResponse {
+    let template = Index {
+        language: "".to_string(),
+    };
+    HtmlTemplate(template)
 }
 
-async fn retrieve_paste(req: Request<()>) -> Result<Response, tide::Error> {
-    let paste_id = req.param("id")?;
-    if req.header("Accept").unwrap() == "text/plain" {
-        let mut res = Response::new(StatusCode::Ok);
-        let body = Body::from_file(format!("pastes/{}", paste_id)).await?;
-        res.set_body(body);
-        return Ok(res);
-    }
-
-    let metadata = fs::read_to_string(format!("pastes/metadata/{}", paste_id)).await?;
-
-    let res: Response = Index {
-        language: &metadata,
-    }
-    .into();
-    Ok(res)
-}
-
-async fn create_paste(req: Request<()>) -> Result<Response, tide::Error> {
+async fn create(input: String, headers: HeaderMap) -> impl IntoResponse {
     let id = PasteId::new(7);
-    // Save metadata
 
-    let file_type = req.header("X-language").unwrap();
-    // println!("File Type =={}", );
+    let file_type = headers.get("X-language").unwrap();
     fs::write(
         format!("pastes/metadata/{}", id),
-        file_type.get(0).unwrap().as_str(),
+        file_type.to_str().unwrap(),
     )
-    .await?;
-    let file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .open(&format!("pastes/{}", id))
-        .await?;
-    let _bytes_written = io::copy(req, file).await?;
-    let resp = Response::builder(StatusCode::Ok)
-        .body(Body::from_string(format!("{}", id)))
-        .build();
-    Ok(resp)
+    .unwrap();
+
+    fs::write(format!("pastes/{}", id), input).unwrap();
+    (StatusCode::CREATED, format!("{} {}", id, 0)) // hardcoding bytes_written to 0 for now, calculate it later
+}
+
+// #[debug_handler]
+async fn retrieve_paste(Path(paste_id): Path<String>, _headers: HeaderMap) -> impl IntoResponse {
+    let content = fs::read_to_string(format!("pastes/{}", paste_id)).unwrap();
+    return (StatusCode::OK, content);
+}
+
+async fn retrieve_paste_doc(Path(paste_id): Path<String>) -> impl IntoResponse {
+    let metadata = fs::read_to_string(format!("pastes/metadata/{}", paste_id)).unwrap();
+    let page = Index { language: metadata };
+    HtmlTemplate(page)
 }
--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,20 @@
 // deno-lint-ignore-file
 function create(data, language) {
-    return fetch("/", {
-        method: "POST",
-        headers: { "X-Language": language },
-        body: data,
-    }).then(function (res) {
-        return res.text().then(function (text) {
-            if (res.status !== 200) {
-                throw new Error(text);
-            }
-            var uuidBytes = text.split(" ");
-            var uuid = uuidBytes[0];
-            var bytes = uuidBytes[1];
-            return [uuid, bytes];
-        });
+  return fetch("/", {
+    method: "POST",
+    headers: { "X-Language": language }, // , "content-type": "application/json"
+    body: data,
+  }).then(function (res) {
+    return res.text().then(function (text) {
+      if (res.status !== 201) {
+        throw new Error(text);
+      }
+      var uuidBytes = text.split(" ");
+      var uuid = uuidBytes[0];
+      var bytes = uuidBytes[1];
+      return [uuid, bytes];
     });
+  });
 }
 
 // const langs = [
@@ -92,170 +92,167 @@ var main = document.querySelector("#main");
 var isNew = location.pathname === "/";
 
 function goto() {
-    const line = prompt("Enter line number to goto:");
-    const lineNumber = Number(line);
-    editor.setPosition({ lineNumber, column: 0 });
-    editor.focus();
+  const line = prompt("Enter line number to goto:");
+  const lineNumber = Number(line);
+  editor.setPosition({ lineNumber, column: 0 });
+  editor.focus();
 }
 
 function settabs() {
-    const tabWidth = prompt("Enter tab width:") || 4;
-    const model = editor.getModel();
-    const currPos = editor.getScrollTop();
-    console.log("Scroll pos is", currPos);
-    model.updateOptions({ tabSize: tabWidth });
-    editor.trigger("", "editor.action.formatDocument");
-    document.getElementById("lineno").innerText = `Spaces: ${tabWidth}`;
-    editor.focus();
-    setTimeout(() => {
-        editor.setScrollTop(currPos);
-    }, 100);
+  const tabWidth = prompt("Enter tab width:") || 4;
+  const model = editor.getModel();
+  const currPos = editor.getScrollTop();
+  model.updateOptions({ tabSize: tabWidth });
+  editor.trigger("", "editor.action.formatDocument");
+  document.getElementById("lineno").innerText = `Spaces: ${tabWidth}`;
+  editor.focus();
+  setTimeout(() => {
+    editor.setScrollTop(currPos);
+  }, 100);
 }
 
 function setLang(e) {
-    editor.trigger("Source", "vs.editor.ICodeEditor:1:set-language");
+  editor.trigger("Source", "vs.editor.ICodeEditor:1:set-language");
 }
 
 function fork() {
-    if (isNew) {
-        return;
-    }
-    history.pushState({}, document.title, "/");
-    editor.updateOptions({ readOnly: false });
-    isNew = true;
+  if (isNew) {
+    return;
+  }
+  history.pushState({}, document.title, "/");
+  editor.updateOptions({ readOnly: false });
+  isNew = true;
 }
 
 function save() {
-    if (!isNew) {
-        return;
-    }
-    var data = editor.getValue();
-    var language = ((editor.getModel() || {})._languageIdentifier || {})
-        .language;
-    create(data, language)
-        .then(function (uuidBytes) {
-            var uuid = uuidBytes[0];
-            var bytes = uuidBytes[1];
-            localStorage.removeItem("paste");
-            location.href = "/" + uuid;
-            return;
-        })
-        .catch(function (err) {
-            var msg = err.message;
-            editor.setValue(editor.getValue() + "\n// Error: " + msg);
-        });
+  if (!isNew) {
+    return;
+  }
+  var data = editor.getValue();
+  var language = ((editor.getModel() || {})._languageIdentifier || {}).language;
+  create(data, language)
+    .then(function (uuidBytes) {
+      var uuid = uuidBytes[0];
+      var bytes = uuidBytes[1];
+      localStorage.removeItem("paste");
+      location.href = "/" + uuid;
+      return;
+    })
+    .catch(function (err) {
+      var msg = err.message;
+      editor.setValue(editor.getValue() + "\n// Error: " + msg);
+    });
 }
 
 require(["vs/editor/editor.main"], function () {
-    if (!(main instanceof HTMLElement)) {
-        return;
-    }
-    var editor = monaco.editor.create(main, {
-        theme: "vs-dark",
-        wordWrap: "on",
-        readOnly: !isNew,
-        theme: "vs-dark",
-        fontFamily: "Fira Code",
-        cursorBlinking: "smooth",
-        renderWhitespace: "all",
-    });
-    window.editor = editor;
-    window.addEventListener("resize", () => {
-        editor.layout();
-    });
-    // Get cursor
-    editor.onDidChangeCursorPosition((e) => {
-        document.getElementById(
-            "lines"
-        ).innerText = `Ln ${e.position.lineNumber}, Col ${e.position.column}`;
-    });
-    editor.onDidChangeModelContent(() => {
-        const error = monaco.editor.getModelMarkers({});
-        let errordiv = document.querySelector(".error-counter");
-        let warndiv = document.querySelector(".warn-counter");
-        if (error.length) {
-            errordiv.innerHTML = error.length;
-            warndiv.innerHTML = error[0].message;
-            // console.log();
-        } else {
-            errordiv.innerHTML = 0;
-            warndiv.innerHTML = "";
-        }
-    });
-    document.addEventListener("keydown", function (e) {
-        if (!(e.ctrlKey || e.metaKey)) {
-            return;
-        }
-        if (e.code === "KeyS") {
-            if (!isNew) {
-                return;
-            }
-            e.preventDefault();
-            save();
-            return;
-        }
-        if (e.code === "KeyF" && e.shiftKey) {
-            if (isNew) {
-                return;
-            }
-            e.preventDefault();
-            fork();
-            return;
-        }
-        localStorage.setItem("paste", editor.getValue());
-    });
-    editor.addAction({
-        id: "set-language",
-        label: "Set language",
-        keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_L],
-        run: function (ed) {
-            var lang = prompt("Set language");
-            var model = ed.getModel();
-            if (!model || !lang) {
-                return;
-            }
-            monaco.editor.setModelLanguage(model, lang);
-            localStorage.setItem("language", lang);
-            document.getElementById("setlang").innerText = lang;
-        },
-    });
-    if (!isNew) {
-        var fileLang = location.pathname.split(".");
-        var file = fileLang[0];
-        var lang =
-            fileLang[1] ||
-            document
-                .querySelector('meta[http-equiv="X-Language"]')
-                .getAttribute("content");
-        if (lang) {
-            var model = editor.getModel();
-            if (model) {
-                monaco.editor.setModelLanguage(model, lang);
-            }
-        }
-        editor.setValue("// Loading...");
-        fetch(file, {
-            headers: {
-                Accept: "text/plain",
-            },
-        })
-            .then(function (res) {
-                return res.text();
-            })
-            .then(function (text) {
-                editor.setValue(text);
-            });
+  if (!(main instanceof HTMLElement)) {
+    return;
+  }
+  var editor = monaco.editor.create(main, {
+    theme: "vs-dark",
+    wordWrap: "on",
+    readOnly: !isNew,
+    theme: "vs-dark",
+    fontFamily: "Fira Code",
+    cursorBlinking: "smooth",
+    renderWhitespace: "all",
+  });
+  window.editor = editor;
+  window.addEventListener("resize", () => {
+    editor.layout();
+  });
+  // Get cursor
+  editor.onDidChangeCursorPosition((e) => {
+    document.getElementById(
+      "lines"
+    ).innerText = `Ln ${e.position.lineNumber}, Col ${e.position.column}`;
+  });
+  editor.onDidChangeModelContent(() => {
+    const error = monaco.editor.getModelMarkers({});
+    let errordiv = document.querySelector(".error-counter");
+    let warndiv = document.querySelector(".warn-counter");
+    if (error.length) {
+      errordiv.innerHTML = error.length;
+      warndiv.innerHTML = error[0].message;
     } else {
-        var stored = localStorage.getItem("paste");
-        if (stored) {
-            editor.setValue(stored);
-        }
-        var lang = localStorage.getItem("language");
-        var model = editor.getModel();
-        if (lang) {
-            if (model) {
-                monaco.editor.setModelLanguage(model, lang);
-            }
-        }
+      errordiv.innerHTML = 0;
+      warndiv.innerHTML = "";
     }
+  });
+  document.addEventListener("keydown", function (e) {
+    if (!(e.ctrlKey || e.metaKey)) {
+      return;
+    }
+    if (e.code === "KeyS") {
+      if (!isNew) {
+        return;
+      }
+      e.preventDefault();
+      save();
+      return;
+    }
+    if (e.code === "KeyF" && e.shiftKey) {
+      if (isNew) {
+        return;
+      }
+      e.preventDefault();
+      fork();
+      return;
+    }
+    localStorage.setItem("paste", editor.getValue());
+  });
+  editor.addAction({
+    id: "set-language",
+    label: "Set language",
+    keybindings: [monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_L],
+    run: function (ed) {
+      var lang = prompt("Set language");
+      var model = ed.getModel();
+      if (!model || !lang) {
+        return;
+      }
+      monaco.editor.setModelLanguage(model, lang);
+      localStorage.setItem("language", lang);
+      document.getElementById("setlang").innerText = lang;
+    },
+  });
+  if (!isNew) {
+    var fileLang = location.pathname.split(".");
+    var file = fileLang[0];
+    var lang =
+      fileLang[1] ||
+      document
+        .querySelector('meta[http-equiv="X-Language"]')
+        .getAttribute("content");
+    if (lang) {
+      var model = editor.getModel();
+      if (model) {
+        monaco.editor.setModelLanguage(model, lang);
+      }
+    }
+    editor.setValue("// Loading...");
+    fetch("api" + file, {
+      headers: {
+        Accept: "text/plain",
+      },
+    })
+      .then(function (res) {
+        return res.text();
+      })
+      .then(function (text) {
+        editor.setValue(text);
+      });
+  } else {
+    var stored = localStorage.getItem("paste");
+    if (stored) {
+      editor.setValue(stored);
+    }
+    var lang = localStorage.getItem("language");
+    var model = editor.getModel();
+    if (lang) {
+      if (model) {
+        monaco.editor.setModelLanguage(model, lang);
+      }
+    }
+  }
 });
